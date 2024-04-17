742b91557dc10aed2ea97fc8627ec783fa64ee8b
nquinbla <nquinbla@myuax.com>
2024-04-17 19:45:21 +0200

    [Módulo 1] ajustes pt11

--- src/main/java/A_GestiónDatosDinámicos/GUI_GestiónDatosDinámicos.java
+++ src/main/java/A_GestiónDatosDinámicos/GUI_GestiónDatosDinámicos.java
@@ -9,7 +9,8 @@ public class GUI_GestiónDatosDinámicos extends JFrame {
     private GestiónDatosDinámicos gestionDatosDinamicos;
     private JTextField primerElementoField;
     private JTextField segundoElementoField;
-    private JTextArea parejasArea;
+    private JTextField tercerElementoField; // Nuevo campo para la matriz
+    private JTextArea datosArea;
 
     public GUI_GestiónDatosDinámicos(String tipo) {
         gestionDatosDinamicos = new GestiónDatosDinámicos();
@@ -21,72 +22,81 @@ public class GUI_GestiónDatosDinámicos extends JFrame {
 
         primerElementoField = new JTextField(10);
         segundoElementoField = new JTextField(10);
+        tercerElementoField = new JTextField(10); // Nuevo campo para la matriz
         JButton agregarButton = new JButton("Agregar " + tipo);
         JButton eliminarButton = new JButton("Eliminar " + tipo);
         JButton modificarButton = new JButton("Modificar " + tipo);
-        parejasArea = new JTextArea(10, 30);
+        datosArea = new JTextArea(10, 30);
 
-        agregarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    int primerElemento = Integer.parseInt(primerElementoField.getText());
-                    int segundoElemento = Integer.parseInt(segundoElementoField.getText());
-                    Pareja pareja = new Pareja(primerElemento, segundoElemento);
-                    gestionDatosDinamicos.agregarPareja(pareja);
-                    actualizarParejasArea();
-                } catch (NumberFormatException ex) {
-                    JOptionPane.showMessageDialog(null, "Por favor, ingrese solo números enteros.");
-                }
-            }
-        });
-
-        eliminarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    int primerElemento = Integer.parseInt(primerElementoField.getText());
-                    int segundoElemento = Integer.parseInt(segundoElementoField.getText());
-                    Pareja pareja = new Pareja(primerElemento, segundoElemento);
-                    gestionDatosDinamicos.eliminarPareja(pareja);
-                    actualizarParejasArea();
-                } catch (NumberFormatException ex) {
-                    JOptionPane.showMessageDialog(null, "Por favor, ingrese solo números enteros.");
-                }
-            }
-        });
-
-        modificarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    int primerElemento = Integer.parseInt(primerElementoField.getText());
-                    int segundoElemento = Integer.parseInt(segundoElementoField.getText());
-                    Pareja pareja = new Pareja(primerElemento, segundoElemento);
-                    int index = gestionDatosDinamicos.getParejas().indexOf(pareja);
-                    if (index != -1) {
-                        int nuevoPrimerElemento = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el nuevo primer elemento de la pareja"));
-                        int nuevoSegundoElemento = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el nuevo segundo elemento de la pareja"));
-                        Pareja nuevaPareja = new Pareja(nuevoPrimerElemento, nuevoSegundoElemento);
-                        gestionDatosDinamicos.modificarPareja(index, nuevaPareja);
+        if (tipo.equals("parejas")) {
+            agregarButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    try {
+                        int primerElemento = Integer.parseInt(primerElementoField.getText());
+                        int segundoElemento = Integer.parseInt(segundoElementoField.getText());
+                        Pareja pareja = new Pareja(primerElemento, segundoElemento);
+                        gestionDatosDinamicos.agregarPareja(pareja);
                         actualizarParejasArea();
-                    } else {
-                        JOptionPane.showMessageDialog(null, "La pareja no se encuentra en la lista.");
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(null, "Por favor, ingrese solo números enteros.");
                     }
-                } catch (NumberFormatException ex) {
-                    JOptionPane.showMessageDialog(null, "Por favor, ingrese solo números enteros.");
                 }
-            }
-        });
+            });
+
+            eliminarButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    try {
+                        int primerElemento = Integer.parseInt(primerElementoField.getText());
+                        int segundoElemento = Integer.parseInt(segundoElementoField.getText());
+                        Pareja pareja = new Pareja(primerElemento, segundoElemento);
+                        gestionDatosDinamicos.eliminarPareja(pareja);
+                        actualizarParejasArea();
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(null, "Por favor, ingrese solo números enteros.");
+                    }
+                }
+            });
+
+            modificarButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    try {
+                        int primerElemento = Integer.parseInt(primerElementoField.getText());
+                        int segundoElemento = Integer.parseInt(segundoElementoField.getText());
+                        Pareja pareja = new Pareja(primerElemento, segundoElemento);
+                        int index = gestionDatosDinamicos.getParejas().indexOf(pareja);
+                        if (index != -1) {
+                            int nuevoPrimerElemento = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el nuevo primer elemento de la pareja"));
+                            int nuevoSegundoElemento = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el nuevo segundo elemento de la pareja"));
+                            Pareja nuevaPareja = new Pareja(nuevoPrimerElemento, nuevoSegundoElemento);
+                            gestionDatosDinamicos.modificarPareja(index, nuevaPareja);
+                            actualizarParejasArea();
+                        } else {
+                            JOptionPane.showMessageDialog(null, "La pareja no se encuentra en la lista.");
+                        }
+                    } catch (NumberFormatException ex) {
+                        JOptionPane.showMessageDialog(null, "Por favor, ingrese solo números enteros.");
+                    }
+                }
+            });
+        } else if (tipo.equals("matrices")) {
+            // Aquí va el código para agregar los ActionListener a los botones para trabajar con matrices
+        }
 
         add(new JLabel("Primer Elemento:"));
         add(primerElementoField);
         add(new JLabel("Segundo Elemento:"));
         add(segundoElementoField);
+        if (tipo.equals("matrices")) {
+            add(new JLabel("Tercer Elemento:")); // Nuevo campo para la matriz
+            add(tercerElementoField);
+        }
         add(agregarButton);
         add(eliminarButton);
         add(modificarButton);
-        add(new JScrollPane(parejasArea));
+        add(new JScrollPane(datosArea));
 
         setSize(550, 300);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
