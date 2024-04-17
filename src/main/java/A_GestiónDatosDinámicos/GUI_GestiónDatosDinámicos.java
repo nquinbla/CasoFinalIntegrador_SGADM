19812cf7982d36909a55e538df4f664bf49e39de
nquinbla <nquinbla@myuax.com>
2024-04-17 18:59:19 +0200

    [Módulo 1] ajustes pt2

--- src/main/java/A_GestiónDatosDinámicos/GUI_GestiónDatosDinámicos.java
+++ src/main/java/A_GestiónDatosDinámicos/GUI_GestiónDatosDinámicos.java
@@ -19,6 +19,8 @@ public class GUI_GestiónDatosDinámicos extends JFrame {
         primerElementoField = new JTextField(10);
         segundoElementoField = new JTextField(10);
         JButton agregarButton = new JButton("Agregar Pareja");
+        JButton eliminarButton = new JButton("Eliminar Pareja");
+        JButton modificarButton = new JButton("Modificar Pareja");
         parejasArea = new JTextArea(10, 30);
 
         agregarButton.addActionListener(new ActionListener() {
@@ -32,11 +34,36 @@ public class GUI_GestiónDatosDinámicos extends JFrame {
             }
         });
 
+        eliminarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int primerElemento = Integer.parseInt(primerElementoField.getText());
+                int segundoElemento = Integer.parseInt(segundoElementoField.getText());
+                Pareja pareja = new Pareja(primerElemento, segundoElemento);
+                gestionDatosDinamicos.eliminarPareja(pareja);
+                actualizarParejasArea();
+            }
+        });
+
+        modificarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int index = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el índice de la pareja a modificar"));
+                int primerElemento = Integer.parseInt(primerElementoField.getText());
+                int segundoElemento = Integer.parseInt(segundoElementoField.getText());
+                Pareja nuevaPareja = new Pareja(primerElemento, segundoElemento);
+                gestionDatosDinamicos.modificarPareja(index, nuevaPareja);
+                actualizarParejasArea();
+            }
+        });
+
         add(new JLabel("Primer Elemento:"));
         add(primerElementoField);
         add(new JLabel("Segundo Elemento:"));
         add(segundoElementoField);
         add(agregarButton);
+        add(eliminarButton);
+        add(modificarButton);
         add(new JScrollPane(parejasArea));
 
         setSize(400, 300);
