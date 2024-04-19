c58d4a9c302200147e8f317000b2901de0aa15d0
nquinbla <nquinbla@myuax.com>
2024-04-19 13:51:50 +0200

    [Módulo 2]  Clase Venta.java

--- src/main/java/B_AnálisisYOrganizaciónInfo/Venta.java
+++ src/main/java/B_AnálisisYOrganizaciónInfo/Venta.java
@@ -1,4 +1,27 @@
 package B_AnálisisYOrganizaciónInfo;
 
+import java.util.Date;
+
 public class Venta {
+    private Date fecha;
+    private int cantidad;
+    private String cliente;
+
+    public Venta(Date fecha, int cantidad, String cliente) {
+        this.fecha = fecha;
+        this.cantidad = cantidad;
+        this.cliente = cliente;
+    }
+
+    public Date getFecha() {return fecha;}
+
+    public int getCantidad() {return cantidad;}
+
+    public String getCliente() {return cliente;}
+
+    public void setFecha(Date fecha) {this.fecha = fecha;}
+
+    public void setCantidad(int cantidad) {this.cantidad = cantidad;}
+
+    public void setCliente(String cliente) {this.cliente = cliente;}
 }
