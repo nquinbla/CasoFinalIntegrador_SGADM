68f6f705a74b6773831210b2fd950e6c9243978f
nquinbla <nquinbla@myuax.com>
2024-04-21 17:30:31 +0200

    Main código pt1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,20 @@
-import javax.swing.*;
+import A_GestiónDatosDinámicos.GUI_Selección;
+import B_AnálisisYOrganizaciónInfo.GUI_Ventas;
+import C_MapasYAsociaciónDatos.GUI_Relaciones;
+import D_IndexYVisualArchivos.GUI_Archivos;
 
-public class Main extends JFrame {
+public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Aplicación de Gestión y Análisis de Datos Multidimensionales");
-            frame.setSize(1000, 600);
-            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-            frame.setVisible(true);
-        });
+        // Crear instancias de las clases
+        GUI_Selección gui_seleccion = new GUI_Selección();
+        GUI_Ventas gui_ventas = new GUI_Ventas();
+        GUI_Relaciones gui_relaciones = new GUI_Relaciones();
+        GUI_Archivos gui_archivos = new GUI_Archivos();
+
+        // Mostrar las ventanas de la interfaz de usuario
+        gui_seleccion.setVisible(true);
+        gui_ventas.setVisible(true);
+        gui_relaciones.setVisible(true);
+        gui_archivos.setVisible(true);
     }
 }
