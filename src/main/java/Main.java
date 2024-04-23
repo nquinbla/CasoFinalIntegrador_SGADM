dad5e060dd3e03d4cad46b66e92367f24e324468
nquinbla <nquinbla@myuax.com>
2024-04-23 14:41:26 +0200

    main pt5 + imagenes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,30 +13,33 @@ public class Main extends JFrame {
         setTitle("Sistema de Gestión Y Análisis de Datos Multidimensionales");
 
         DecoPanel decoPanel = new DecoPanel();
-        decoPanel.setLayout(new BoxLayout(decoPanel, BoxLayout.Y_AXIS));
+        decoPanel.setLayout(new BorderLayout());
 
-        JPanel buttonPanel = new JPanel(new FlowLayout());
+        JPanel buttonPanel = new JPanel(new GridBagLayout());
         buttonPanel.setOpaque(false);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.anchor = GridBagConstraints.NORTH;
+        gbc.weighty = 1;
 
         JButton gestionDatosDinamicosButton = new JButton("Gestión de Datos Dinámicos \uD83D\uDCC8\u200B\uD83D\uDCC9\u200B");
         gestionDatosDinamicosButton.addActionListener(e -> new Main_A());
+        buttonPanel.add(gestionDatosDinamicosButton, gbc);
 
         JButton analisisOrganizacionButton = new JButton("Análisis y Organización de Información \uD83D\uDDC4\uFE0F\u200Bℹ\uFE0F\u200B");
         analisisOrganizacionButton.addActionListener(e -> new Main_B());
+        buttonPanel.add(analisisOrganizacionButton, gbc);
 
         JButton mapasAsociacionButton = new JButton("Mapas y Asociación de Datos \uD83D\uDDFA\uFE0F\u200B\uD83D\uDCCA\u200B");
         mapasAsociacionButton.addActionListener(e -> new Main_C());
+        buttonPanel.add(mapasAsociacionButton, gbc);
 
         JButton indexadorVisualizacionButton = new JButton("Indexador y Visualización de Archivos \uD83D\uDC41\uFE0F\u200B\uD83D\uDDC3\uFE0F\u200B");
         indexadorVisualizacionButton.addActionListener(e -> new Main_D());
+        buttonPanel.add(indexadorVisualizacionButton, gbc);
 
-        buttonPanel.add(gestionDatosDinamicosButton);
-        buttonPanel.add(analisisOrganizacionButton);
-        buttonPanel.add(mapasAsociacionButton);
-        buttonPanel.add(indexadorVisualizacionButton);
-
-        decoPanel.add(Box.createVerticalGlue());
-        decoPanel.add(buttonPanel);
+        decoPanel.add(buttonPanel, BorderLayout.EAST);
 
         add(decoPanel, BorderLayout.CENTER);
 
