3191ac887817b1f071651b44756fe76f6c09e275
nquinbla <nquinbla@myuax.com>
2024-04-23 13:46:22 +0200

    revisión main A pt1

--- src/main/java/A_GestiónDatosDinámicos/Main_A.java
+++ src/main/java/A_GestiónDatosDinámicos/Main_A.java
@@ -8,11 +8,28 @@ import java.awt.event.ActionListener;
 public class Main_A extends JFrame {
     public Main_A() {
         setLayout(new FlowLayout());
+        getContentPane().setBackground(Color.LIGHT_GRAY);
+
+        Font buttonFont = new Font("Arial", Font.BOLD, 14);
+        Color buttonTextColor = Color.WHITE;
+        Color buttonBackgroundColor = Color.DARK_GRAY;
 
         JLabel instructionLabel = new JLabel("Por favor, seleccione el campo con el que quiere trabajar");
+
         JButton parejasButton = new JButton("Trabajar con Parejas");
+        parejasButton.setFont(buttonFont);
+        parejasButton.setForeground(buttonTextColor);
+        parejasButton.setBackground(buttonBackgroundColor);
+
         JButton matricesButton = new JButton("Trabajar con Matrices");
+        matricesButton.setFont(buttonFont);
+        matricesButton.setForeground(buttonTextColor);
+        matricesButton.setBackground(buttonBackgroundColor);
+
         JButton cadenasButton = new JButton("Trabajar con Cadenas");
+        cadenasButton.setFont(buttonFont);
+        cadenasButton.setForeground(buttonTextColor);
+        cadenasButton.setBackground(buttonBackgroundColor);
 
         parejasButton.addActionListener(new ActionListener() {
             @Override
