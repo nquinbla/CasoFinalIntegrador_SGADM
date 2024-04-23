f692dc56205e013705be90c98ae902e80bce2767
nquinbla <nquinbla@myuax.com>
2024-04-23 16:11:13 +0200

    main pt19

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,18 +16,21 @@ public class Main extends JFrame {
         decoPanel.setLayout(new BorderLayout());
 
         JPanel titlePanel = new JPanel(new GridBagLayout());
-        titlePanel.setOpaque(false); // Hacer que titlePanel sea transparente
+        titlePanel.setOpaque(false);
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridwidth = GridBagConstraints.REMAINDER;
         gbc.anchor = GridBagConstraints.CENTER;
         JLabel titleLabel = new JLabel("<html><h1 style='font-size: 60px;'><font color='black'>U</font><font color='black'>A</font><font color='#3679FA'>X</font></h1></html>");
+        titleLabel.setOpaque(false);
         titlePanel.add(titleLabel, gbc);
 
-        gbc.insets = new Insets(0, 0, 0, 0); // Reducir el espacio vertical
+        gbc.insets = new Insets(0, 0, 0, 0);
         JLabel universityLabel = new JLabel("<html><h2 style='font-size: 16px;'>Universidad Alfonso <font color='#3679FA'>X</font> el Sabio</h2></html>");
+        universityLabel.setOpaque(false);
         titlePanel.add(universityLabel, gbc);
 
         JLabel subtitleLabel = new JLabel("<html><h2 style='font-size: 20px;'>Sistema de Gestión y Análisis de Datos</h2></html>");
+        subtitleLabel.setOpaque(false);
         titlePanel.add(subtitleLabel, gbc);
 
         JPanel buttonPanel = new JPanel(new GridBagLayout());
@@ -37,27 +40,32 @@ public class Main extends JFrame {
         gbc.gridwidth = GridBagConstraints.REMAINDER;
         gbc.anchor = GridBagConstraints.NORTH;
         gbc.weighty = 0;
-        gbc.insets = new Insets(10, 0, 10, 0); // Agregar espacio vertical entre los botones
+        gbc.insets = new Insets(10, 0, 10, 0);
         gbc.ipady = 0;
 
         JButton gestionDatosDinamicosButton = new JButton("Gestión de Datos Dinámicos \uD83D\uDCC8\u200B\uD83D\uDCC9\u200B");
+        gestionDatosDinamicosButton.setOpaque(false);
         gestionDatosDinamicosButton.addActionListener(e -> new Main_A());
         buttonPanel.add(gestionDatosDinamicosButton, gbc);
 
         JButton analisisOrganizacionButton = new JButton("Análisis y Organización de Información \uD83D\uDDC4\uFE0F\u200Bℹ\uFE0F\u200B");
+        analisisOrganizacionButton.setOpaque(false);
         analisisOrganizacionButton.addActionListener(e -> new Main_B());
         buttonPanel.add(analisisOrganizacionButton, gbc);
 
         JButton mapasAsociacionButton = new JButton("Mapas y Asociación de Datos \uD83D\uDDFA\uFE0F\u200B\uD83D\uDCCA\u200B");
+        mapasAsociacionButton.setOpaque(false);
         mapasAsociacionButton.addActionListener(e -> new Main_C());
         buttonPanel.add(mapasAsociacionButton, gbc);
 
         JButton indexadorVisualizacionButton = new JButton("Indexador y Visualización de Archivos \uD83D\uDC41\uFE0F\u200B\uD83D\uDDC3\uFE0F\u200B");
+        indexadorVisualizacionButton.setOpaque(false);
         indexadorVisualizacionButton.addActionListener(e -> new Main_D());
         buttonPanel.add(indexadorVisualizacionButton, gbc);
 
         JPanel centerPanel = new JPanel();
         centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+        centerPanel.setOpaque(false);
         centerPanel.add(titlePanel);
         centerPanel.add(buttonPanel);
         decoPanel.add(centerPanel, BorderLayout.CENTER);
